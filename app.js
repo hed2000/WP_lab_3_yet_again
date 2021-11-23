ea307b26b64c69a91f25b19ab727f048feb5788c
hed2000 <91254825+hed2000@users.noreply.github.com>
2021-11-23 01:52:44 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require("express");
 //creating app
 const app = express();
-//send an HTTP response when receiving HTTP GET /
+//handling static HTML and EJS templates
 app.use(express.static("public"));
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
-  res.sendFile("index.html", { root: __dirname });
+  res.render("index"); //no need for ejs extension
 });
+//route for contacts
+app.get("/contacts", (req, res) => {
+  res.render("contacts");
+});
+//route for register
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+//route for login
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
 //make the app listen on port
 const port = process.argv[2] || process.env.PORT || 3000;
 const server = app.listen(port, () => {
