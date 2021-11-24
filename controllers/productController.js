9e3b1fb63cf57dc8ce7865bec0cc051958c32504
hed2000 <91254825+hed2000@users.noreply.github.com>
2021-11-24 16:31:58 +0000

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,24 +1,22 @@
 const getCatalogue = (request, response) => {
   const catalogServices = require("../services/productServices");
   catalogServices.searchService(function (err, rows) {
-    response.json(rows);
-    response.end();
+    response.render("catalog", { products: rows });
   });
 };
 
 const getProductByID = (request, response) => {
   const catalogServices = require("../services/productServices");
-  let reference = request.params.reference;
+  let reference = request.params.id;
   catalogServices.searchIDService(reference, function (err, rows) {
-    response.json(rows);
-    response.end();
+    response.render("article", { products: rows });
   });
 };
 
 const getProductsByCategory = (request, response) => {
   const catalogServices = require("../services/productServices");
   let reference = request.params.category;
-  catalogServices.searchCategoryService(category, function (err, rows) {
+  catalogServices.searchCategoryService(reference, function (err, rows) {
     response.json(rows);
     response.end();
   });
