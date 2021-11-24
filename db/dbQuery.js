37508a7b94bdc5a4ef08ac01aaf57ca534f724b3
hed2000 <91254825+hed2000@users.noreply.github.com>
2021-11-24 16:31:46 +0000

    Update dbQuery.js

--- db/dbQuery.js
+++ db/dbQuery.js
@@ -1,12 +1,12 @@
 const mysql = require("mysql");
-const databasename = "sql6440943";
+const databasename = "sql4451547";
 
 var pool = mysql.createPool({
   connectionLimit: 100,
-  host: "sql6.freemysqlhosting.net",
-  user: "sql6440943",
-  password: "",
-  database: "sql6440943",
+  host: "sql4.freemysqlhosting.net",
+  user: "sql4451547",
+  password: "S7IXevekHV",
+  database: "sql4451547",
   debug: true
 });
 
